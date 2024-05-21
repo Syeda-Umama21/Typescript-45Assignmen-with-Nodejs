e8f2f0ff421532e44346ec466e11a82a4282b0a6
Umama-Shah <165512076+Umama-Shah@users.noreply.github.com>
2024-05-21 16:05:06 +0500

    Add files via upload

---
+++ 02/main.ts
@@ -0,0 +1,9 @@
+//Qs.no.02
+/*
+2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+would you like to learn some Python today?”
+*/
+
+let firstName: string = "Syeda Umama";
+console.log (`Hello ${firstName}, would you like to learn some Typescript today?`);
+
---
+++ 05/main.ts
@@ -0,0 +1,9 @@
+//Qs.no.05
+/*
+5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+Then compose your message and store it in a new variable called message. Print your message.*/
+
+let famousPersons : string = "Bill Gates";
+let famQuote : string = "Patience is a key element of success.";
+console.log(`${famousPersons} ones said,"${famQuote}"`);
+
