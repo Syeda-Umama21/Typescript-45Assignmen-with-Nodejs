183dd03e14cfd839dc3458fdef347d36af06b795
Umama-Shah <165512076+Umama-Shah@users.noreply.github.com>
2024-05-21 16:13:50 +0500

    Add files via upload

---
+++ 27/main.js
@@ -0,0 +1,41 @@
+"use strict";
+//Qs.no 27
+/* . Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+*/
+//Define Variable
+let alienColor = "green";
+//Using If and Else if Statements.
+if (alienColor === "green") {
+    console.log("(Version 1)Player just earn 5 point.");
+}
+else if (alienColor === "yellow") {
+    console.log("Player just earned 10 points.");
+}
+else if (alienColor === "red") {
+    console.log("Player just earned 15 points..");
+}
+//Version 2
+let alienColor2 = "yellow";
+if (alienColor2 === "green") {
+    console.log("You shot down green alien you have 5 points.");
+}
+else if (alienColor2 === "yellow") {
+    console.log("(Version 2) You shot down yellow alien you have 10 points.");
+}
+else if (alienColor2 === "red") {
+    console.log("You shot down red alien you have 15 points.");
+}
+//Version 3
+let alienColor3 = "red";
+if (alienColor3 === "green") {
+    console.log("You shot down green alien you have 5 points.");
+}
+else if (alienColor3 === "yellow") {
+    console.log("You shot down red alien you have 15 points.");
+}
+else if (alienColor3 === "red") {
+    console.log("(Version 3) You shot down red alien you have 15 points.");
+}
---
+++ 29/main.js
@@ -0,0 +1,27 @@
+"use strict";
+//Qs.no.29
+/* 29. Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check
+ for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit is in your array.
+ If the fruit is in your array, the if block should print a statement,
+such as You really like bananas!
+ */
+//Define array
+let favorite_Fruits = ["Mango", "Orange", "Grapes"];
+//Using If Else Statement
+if (favorite_Fruits.includes("Mango")) {
+    console.log("Mango is one of my favorite fruits");
+}
+if (favorite_Fruits.includes("Orange")) {
+    console.log("Orange is one of my favorite fruits.");
+}
+if (favorite_Fruits.includes("Banana")) {
+    console.log("Banana are not of my favorite fruits.");
+}
+if ((favorite_Fruits.includes("Grapes"))) {
+    console.log("Grapes is one my favorites fruits.");
+}
+if ((favorite_Fruits.includes("Guava"))) {
+    console.log("Guava are not of my favorit fruit.");
+}
