7ab4b7583bedb6e175c581d1f3cf36274a84dad9
Umama-Shah <165512076+Umama-Shah@users.noreply.github.com>
2024-05-21 16:10:57 +0500

    Add files via upload

---
+++ 12/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Qs.no. 12
+/*12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them.
+The text of each message should be the same, but each message should be personalized with the person’s name.*/
+let members = ["Umama", "Bisma", "Atiya"];
+members.forEach(member => console.log(`Hy, ${member} how are you today?`));
---
+++ 14/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Qs.no.14
+/*14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting them to dinner.*/
+let guestList = ["Umama", "Atiya", "Bisma", "Warda"];
+guestList.forEach(oneGuest => console.log(`Hello ${oneGuest}, would you like to dinner with me.`));
